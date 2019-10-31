e9cfffc8ceb49318c583776e7dacf743f55c5582
ljsherlock <developer@ljsherlock.com>
2019-10-31 09:02:37 +0700

    deploy to netify

---
+++ app/components/Card.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { ThemeConsumer } from '../contexts/theme'
+
+export default function Card ({ header, subHeader, avatar, href, name, children }) {
+  return (
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className={`card bg-${theme}`}>
+          <h4 className='header-lg center-text'>
+            {header}
+          </h4>
+          <img 
+            className='avatar'
+            src={avatar}
+            alt={`Avatar for ${name}`}
+          />
+          {subHeader && 
+            <h4 className='center-text'>
+              {header}
+            </h4>
+          }
+          <h2 className='center-text'>
+            <a href={href} className='link'>
+              {name}
+            </a>
+          </h2>
+          {children}
+        </div> 
+      )}
+    </ThemeConsumer>
+  )
+}
+
+Card.prototypes = {
+  header: PropTypes.string.isRequired,
+  subHeader: PropTypes.string,
+  avatar: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+}
---
+++ app/components/Loading.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const style = {
+  content: {
+    fontSize: '35px',
+    position: 'absolute',
+    textAlign: 'center',
+    left: '0',
+    right: '0',
+    margin: '20px auto 0',
+  }
+}
+
+export default class Loading extends React.Component {
+  state = { content: this.props.text }
+
+  componentDidMount () {
+    const { speed, text } = this.props
+
+    this.interval = window.setInterval(() => {
+      this.state.content === text + '...'
+      ? this.setState({ content: text })
+      : this.setState(({ content }) => ({ content: content + '.' }))
+    }, speed)
+  }
+
+  componentWillUnmount () {
+    window.clearInterval(this.interval)
+  }
+  
+  render() {
+    return (
+      <p style={style.content}>
+        {this.state.content}
+      </p>
+    )
+  }
+}
+
+Loading.protoTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+}
+
+Loading.defaultProps = {
+  text: 'Loading',
+  speed: 300
+}
